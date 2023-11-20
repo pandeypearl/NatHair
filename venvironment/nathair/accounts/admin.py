4307a9457d3b4b2fea129756de1f5a73023e0233
Pretty Pandey <prettypandeypearl@gmail.com>
2023-11-20 21:27:02 +0200

    Adding Texture Profile to admin

--- venvironment/nathair/accounts/admin.py
+++ venvironment/nathair/accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile, Profile, HairProfile
+from .models import UserProfile, Profile, HairProfile, TextureProfile
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Profile)
 admin.site.register(HairProfile)
+admin.site.register(TextureProfile)
 
