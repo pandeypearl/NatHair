d05cffcd7fa604d8ee3813ee9df70d4f2cf9888d
Pretty Pandey <prettypandeypearl@gmail.com>
2023-11-21 20:07:12 +0200

    Product list and Porduct detail template update

--- venvironment/nathair/products/urls.py
+++ venvironment/nathair/products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # path('product-list', ProductView.as_view(), name='product_list'),
     # path('product_detail/<pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product_list', views.product_list, name='product_list'),
-    path('product_detail<int:product_id>', views.product_detail, name='product_detail'),
+    path('product_detail/<int:product_id>', views.product_detail, name='product_detail'),
 ]
