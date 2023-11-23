0d148930bf88e99fcc60d1d22d76584ef0384eb3
Pretty Pandey <prettypandeypearl@gmail.com>
2023-11-23 13:01:34 +0200

    Adding draft and posted_at fields to hair routine model and Routines list frontend

--- venvironment/nathair/routines/models.py
+++ venvironment/nathair/routines/models.py
@@ -11,6 +11,12 @@ class HairRoutine(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     notes = models.TextField()
+    is_draft = models.BooleanField(default=True)
+    posted_at = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.is_draft = False
+        self.save()
 
     def __str__(self):
         return '%s - %s' % (self.name, self.user)
--- venvironment/nathair/routines/urls.py
+++ venvironment/nathair/routines/urls.py
@@ -3,5 +3,7 @@ from. import views
 
 urlpatterns = [
     path('create_routine', views.create_routine, name='create_routine'),
+    path('routines_list', views.routines_list, name='routines_list'),
     path('routine_detail/<int:routine_id>', views.routine_detail, name='routine_detail'),
+    path('delete_routine/<int:pk>', views.delete_routine, name='delete_routine'),
 ]
